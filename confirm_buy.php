5f8b687b56d8729e2e6345d0b8c08aa51c87c633
plafer <laferriere.phil@gmail.com>
2014-11-01 18:11:40 -0400

    to pull

--- confirm_buy.php
+++ confirm_buy.php
@@ -1,14 +1,15 @@
 <?php
+    session_start();
 	if(!isset($_SESSION['user']))
 	{
 		header('Location: index.php');
 	}
 	
-	session_start();
+	
 	require_once("lib.php");
+    require_once("database.php");
 	require_once("coinbase-php/lib/Coinbase.php");
-	#TODO Alex: if($_SESSION["user"]'s token is set) {do what is below}
-	#Tokens value for Philippe's account for now
+
     $stmt = $conn->prepare('SELECT access_token, refresh_token, expire_time FROM student WHERE username = ?');
     $stmt->execute(array($_SESSION["user"]));
 
--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 session_start();
 require_once('lib.php');
 
-
 if(!isset($_SESSION['user']))
 {
 	#welcome.php shouldn't initialize a session_start();
