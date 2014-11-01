ab5d6eec794742cc8a85461e8e13ae2dd88b1c6d
plafer <laferriere.phil@gmail.com>
2014-11-01 14:01:15 -0400

    to merge

--- homepage.php
+++ homepage.php
@@ -1,4 +1,21 @@
 <?php
-echo "homepage.php\nhere";
+require_once('lib.php');
+require_once("coinbase-php/lib/Coinbase.php");
+
+if(isset($_GET['code'])) {
+	#User was redirected from coinbase after he accepted our third-party bitcoin app
+
+	$coinbaseOauth = new Coinbase_OAuth($_gCLIENT_ID, $_gCLIENT_SECRET, $_gREDIRECT_URL);
+	$tokens = $coinbaseOauth->getTokens($_GET['code']);
+	# $tokens["access_token"] string
+	# $tokens["refresh_token"] string
+	# $tokens["expire_time"] int
+
+	#TODO Alex: Store tokens that user has can now make transaction
+
+	$coinbase = Coinbase::withOauth($coinbaseOauth, $tokens);
+}
+
+#TODO Alex (or someone): In navbar.php, if user's tokens is set in DB, don't show "Link coinbase account" link 
 
 ?>
